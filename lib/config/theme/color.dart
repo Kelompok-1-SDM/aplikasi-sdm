52a5c41dcd15309f032a6ad6e9ea8dda0ce99520
ahmaduunnail <ahmadunnail0@gmail.com>
2024-09-25 13:50:16 +0700

    🎉 Project setup

---
+++ lib/config/theme/color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+abstract final class ColorPrimary {
+  //Primary
+  static const Color blue = Color(0xff748EFE);
+  static const Color orange = Color(0xffFF6333);
+  static const Color green = Color(0xff13AE85);
+}
+
+abstract final class ColorNeutral {
+  //Neutral
+  static const Color gray = Color(0xff3A3A3A);
+  static const Color black = Color(0xff141414);
+  static const Color background = Color(0xffF2F2F2);
+  static const Color white = Color(0xffFFFFFF);
+}
--- lib/config/theme/theme.dart
+++ lib/config/theme/theme.dart
@@ -1,17 +1,30 @@
+import 'package:aplikasi_manajemen_sdm/config/theme/color.dart';
 import 'package:flutter/material.dart';
 
 ThemeData mainTheme() {
   return ThemeData(
       useMaterial3: true,
-      colorScheme: const ColorScheme.light(brightness: Brightness.light),
+      colorScheme: const ColorScheme.light(
+          primary: ColorPrimary.orange,
+          secondary: ColorPrimary.green,
+          tertiary: ColorPrimary.blue,
+          error: ColorPrimary.orange,
+          surface: ColorNeutral.background,
+          surfaceContainer: ColorNeutral.white,
+          onSurfaceVariant: ColorNeutral.gray),
       textTheme: const TextTheme(
-        displayLarge: TextStyle(
-          fontWeight: FontWeight.w700,
-          fontFamily: 'Plus Jakarta'
-        ),
-        displayMedium: TextStyle(
-          fontWeight: FontWeight.w700,
-          fontFamily: 'Plus Jakarta'
-        ),
-      ));
+          displayLarge: TextStyle(
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Plus Jakarta',
+              color: ColorNeutral.black),
+          displayMedium: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Plus Jakarta',
+              color: ColorNeutral.black),
+          bodyMedium:
+              TextStyle(fontFamily: 'Plus Jakarta', color: ColorNeutral.black),
+          bodySmall: TextStyle(
+              fontFamily: 'Plus Jakarta',
+              fontWeight: FontWeight.w300,
+              color: ColorNeutral.black)));
 }
