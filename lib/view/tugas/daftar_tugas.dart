66f37be644504a14b32432da2168d3923ac5182f
AfifahIda <afifah.ida.a57@gmail.com>
2024-10-16 12:39:49 +0700

    Afifah

--- lib/view/tugas/daftar_tugas.dart
+++ lib/view/tugas/daftar_tugas.dart
@@ -49,11 +49,127 @@ class _DaftarTugasState extends State<DaftarTugas> {
                 CustomIconButton(
                   "assets/icon/location.svg",
                   colorBackground: Colors.transparent,
-                  text: "Auditorium Lt. 8, Teknik Sipil",
+                  text: "Auditorium Lt.8, Teknik Sipil",
                 ),
               ],
-              crumbs: ["ujicoba"],
+              crumbs: ['ujicoba'],
+            ), 
+            CustomCardContent(
+              header: [Text("Kamu akan menghadiri acara ini")],
+              title: "Pengawas ujian masuk maba",
+              actionIcon: [
+                CustomIconButton(
+                  "assets/icon/arrow-45.svg",
+                  colorBackground: ColorNeutral.black,
+                )
+              ],
+              colorBackground: const Color(0xFFFFEB69),
+              descIcon: [
+                CustomIconButton(
+                  "assets/icon/calendar.svg",
+                  colorBackground: Colors.transparent,
+                  text: "12 Januari 2025, 08:00-12:00",
+                ),
+                CustomIconButton(
+                  "assets/icon/location.svg",
+                  colorBackground: Colors.transparent,
+                  text: "Online",
+                ),
+              ],
+              crumbs: ['pengawas', 'ujian', 'online'],
             ),
+            CustomCardContent(
+              header: [Text("Kamu akan menghadiri acara ini")],
+              title: "Bantu dekorasi ruang studio",
+              actionIcon: [
+                CustomIconButton(
+                  "assets/icon/arrow-45.svg",
+                  colorBackground: ColorNeutral.black,
+                )
+              ],
+              colorBackground: ColorPrimary.green,
+              descIcon: [
+                CustomIconButton(
+                  "assets/icon/calendar.svg",
+                  colorBackground: Colors.transparent,
+                  text: "12 Januari 2025, 08:00-12:00",
+                ),
+                CustomIconButton(
+                  "assets/icon/location.svg",
+                  colorBackground: Colors.transparent,
+                  text: "Ruang studio, Lt.8, Gedung Teknik Sipil",
+                ),
+              ],
+              crumbs: ['dekorasi'],
+            ),
+            const Text(
+              "Belum ada penawaran baru",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  decoration: TextDecoration.underline,
+                  fontSize: 20,
+                  color: ColorNeutral.gray),
+            ),SizedBox(
+              height: 200,
+            ),
+            CustomCardContent(
+              header: [Text("Kamu telah menghadiri acara ini")],
+              title: "Seminar di Auper",
+              actionIcon: [
+                CustomIconButton(
+                  "assets/icon/category.svg",
+                  colorBackground: ColorNeutral.black,
+                )
+              ],
+              colorBackground: ColorPrimary.blue,
+              descIcon: [
+                CustomIconButton(
+                  "assets/icon/calendar.svg",
+                  colorBackground: Colors.transparent,
+                  text: "12 Januari, 08:00-12:00",
+                ),
+                CustomIconButton(
+                  "assets/icon/location.svg",
+                  colorBackground: Colors.transparent,
+                  text: "Aula Pertamina",
+                ),
+              ],
+              crumbs: ['pengawas', 'ujian', 'online'],
+            ),
+            CustomCardContent(
+              header: [Text("Kamu telah menghadiri acara ini")],
+              title: "Juri lomba aplikasi",
+              actionIcon: [
+                CustomIconButton(
+                  "assets/icon/category.svg",
+                  colorBackground: ColorNeutral.black,
+                )
+              ],
+              colorBackground: ColorPrimary.blue,
+              descIcon: [
+                CustomIconButton(
+                  "assets/icon/calendar.svg",
+                  colorBackground: Colors.transparent,
+                  text: "12 Juli, Sampai selesai",
+                ),
+                CustomIconButton(
+                  "assets/icon/location.svg",
+                  colorBackground: Colors.transparent,
+                  text: " Gedung Teknik Sipil",
+                ),
+              ],
+              crumbs: ['juri'],
+            ),
+            const Text(
+              "Hanya itu yang kami temukan",
+              style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  decoration: TextDecoration.underline,
+                  fontSize: 20,
+                  color: ColorNeutral.gray),
+            ),SizedBox(
+              height: 200,
+            )
           ],
         ),
       ),
